6fe0ecdace252377379f21996ffca719a43901c1
MaliniSODAS <magnus.nielsen@sodas.ku.dk>
2025-01-14 15:25:46 +0100

    Update script slightly to trigger build

--- main.py
+++ main.py
@@ -12,7 +12,9 @@ if __name__ == "__main__":
     clf = svm.SVC(gamma=0.001)
 
     # Split data into 50% train and 50% test subsets
-    X_train, X_test, y_train, y_test = train_test_split(data, digits.target, test_size=0.5, shuffle=False)
+    X_train, X_test, y_train, y_test = train_test_split(
+        data, digits.target, test_size=0.5, shuffle=False
+    )
 
     # Learn the digits on the train subset
     clf.fit(X_train, y_train)
@@ -20,4 +22,6 @@ if __name__ == "__main__":
     # Predict the value of the digit on the test subset
     predicted = clf.predict(X_test)
 
-    print(f"Classification report for classifier {clf}:\n{metrics.classification_report(y_test, predicted)}\n")
+    print(
+        f"Updated classification report for classifier {clf}:\n{metrics.classification_report(y_test, predicted)}\n"
+    )
